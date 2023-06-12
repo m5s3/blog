ce8be80a1d66c6389b40d396b94ae1b57179e75a
m5s3 <devmyoungsoo2@gmail.com>
2023-06-12 18:54:42 +0900

    임시로 auth 끔

--- blog-api/src/main/kotlin/com/m5s3/blog/config/SecurityConfig.kt
+++ blog-api/src/main/kotlin/com/m5s3/blog/config/SecurityConfig.kt
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.web.SecurityFilterChain
 
@@ -15,26 +16,26 @@ import org.springframework.security.web.SecurityFilterChain
 class SecurityConfig {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-//        http.csrf().disable()
-//        http.headers().frameOptions().sameOrigin().disable()
-//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//        http.authorizeHttpRequests().anyRequest().permitAll()
-//        return http.build()
-        http.authorizeHttpRequests()
-            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-            .requestMatchers(HttpMethod.GET).permitAll()
-            .and()
-            .formLogin()
-            .and()
-            .logout()
-            .logoutSuccessUrl("/")
+        http.csrf().disable()
+        http.headers().frameOptions().sameOrigin().disable()
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http.authorizeHttpRequests().anyRequest().permitAll()
         return http.build()
+//        http.authorizeHttpRequests()
+//            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+//            .requestMatchers(HttpMethod.GET).permitAll()
+//            .and()
+//            .formLogin()
+//            .and()
+//            .logout()
+//            .logoutSuccessUrl("/")
+//        return http.build()
     }
 
-    @Bean
-    fun userDetailService(userDetailsService: UserDetailsService): UserDetailsService =
-         UserDetailsService { userId: String ->
-             userDetailsService
-                .loadUserByUsername(userId)
-        }
+//    @Bean
+//    fun userDetailService(userDetailsService: UserDetailsService): UserDetailsService =
+//         UserDetailsService { userId: String ->
+//             userDetailsService
+//                .loadUserByUsername(userId)
+//        }
 }
